ace9192188d33b34de30bdd4449d4f3d1a914772
yiz <ql3895619@gmail.com>
2021-04-08 15:56:02 +0800

    Feat: add claculator layout

--- src/components/FIndCalculator.vue
+++ src/components/FIndCalculator.vue
@@ -1,6 +1,42 @@
 <template lang="pug">
 .-calculator
-  p  claculator
+  .ui.container.segment
+    .ui.labeled.input
+      .ui.label 現在年齡
+      input(type="number" placeholder="40")
+    .ui.labeled.input
+      .ui.label 退休年齡
+      input(type="number" placeholder="65")
+
+    .ui.divider
+
+    .ui.labeled.input
+      .ui.label 現在薪資
+      input(type="number" placeholder="300000")
+    .ui.labeled.input
+      .ui.label 退休開支
+      input(tyue="number" placeholder="600000")
+
+    .ui.divider
+
+    .ui.labeled.input
+      .ui.label 每年投資金額
+      input(type="number" placeholder="3600000")
+    .ui.labeled.input
+      .ui.label 累積退休資產
+      input(type="number" placeholder="10000000")
+
+    .ui.divider
+
+    .ui.labeled.input
+      .ui.label 高風險資產
+      input(type="number" placeholder="10")
+    .ui.labeled.input
+      .ui.label 低風險資產
+      input(type="number" placeholder="6")
+
+
+
 </template>
 
 <script>
@@ -9,4 +45,12 @@ export default {
 }
 </script>
 
-<style scoped lang="sass"></style>
+<style scoped lang="sass">
+.container
+  max-width: 25em !important
+  display: flex
+  flex-direction: column
+
+.input
+  margin: .2em
+</style>
