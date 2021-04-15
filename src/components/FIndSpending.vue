55b33548a3de761b12bc003143aec3f06ecefd9d
dirty <darby.chang@gmail.com>
2021-04-15 15:22:14 +0800

    Feat: add dawho binding

--- src/components/FIndSpending.vue
+++ src/components/FIndSpending.vue
@@ -3,19 +3,26 @@
 
   #tsps.ui.divided.unstackable.items
     .item
+      .image: img(src='https://bank.sinopac.com/upload/sinopac/picture/16b54be0438000006ff4.png')
+      .content
+        .header 大戶 DAWHO
+        .description 新世代 VIP 數位帳戶
+        .extra
+          span.-spending 本月支出：52,718
+    .item
       .image: img(src='https://play-lh.googleusercontent.com/c9Ef8mWL1hblktrRAil1FaDVEPywqdetidikjj0MvCoeogAiakPxJTICNQuZksDYCA=s360-rw')
       .content
         .header 麻布記帳 Moneybook
         .description 最創新的記帳服務
         .extra
-          span.-spending 本月支出：3231
+          span.-spending 本月支出：3,231
     .item
       .image: img(src='https://play-lh.googleusercontent.com/_vefWfZPhj-_p0oYf5rDdQYGhea2tBmfQ_dxSgagyj7QD-py1BHhxqyxu89hnuN-pY8=s360-rw')
       .content
         .header 發票怪獸
         .description 每次對獎，都有驚喜
         .extra
-          span.-spending 本月支出：1250
+          span.-spending 本月支出：1,250
     .item
       .image: img(src='https://play-lh.googleusercontent.com/ddz2VOlB1TIVJKsXOAV8s-_K4kTjur3oWOS8vPQ9RfPBFwW9BWBDQPofdfoXUWoVDo4=s360-rw')
       .content
